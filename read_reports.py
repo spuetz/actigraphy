b4b8f386a74392d53051b9dc20a1d2abc376a5f3
Sebastian Pütz <spuetz@uos.de>
2022-03-26 22:10:38 +0100

    differ between workdays and weekend, save results to xlsx

--- read_reports.py
+++ read_reports.py
@@ -21,7 +21,7 @@ def parse_header(report_file):
 
             found_header_line = re.findall(header_line_pattern, line)
             if found_header_line:
-                header_lines = i-1
+                header_lines = i - 1
                 break
 
         # print("Subject {}, header lines {}".format(subject, header_lines))
@@ -33,13 +33,14 @@ def date_parser(date, time):
 
 
 def compute_mid_point_of_sleep(row):
-    row['Mpos'] = row['Onset'] + timedelta(minutes=row["Total Sleep Time (TST)"] / 2)
+    row['MPOS'] = row['Onset'] + timedelta(minutes=row["Total Sleep Time (TST)"] / 2)
     return row
 
 
 def read_data(report_file, header_lines):
     with open(report_file) as report:
-        data = pd.read_csv(report, delimiter=',', quotechar='"', header=header_lines, date_parser=date_parser,
+        data = pd.read_csv(report, delimiter=',', quotechar='"', decimal=",",
+                           header=header_lines, date_parser=date_parser,
                            parse_dates={'In Bed': ['In Bed Date', 'In Bed Time'],
                                         'Out Bed': ['Out Bed Date', 'Out Bed Time'],
                                         'Onset': ['Onset Date', 'Onset Time']})
@@ -61,9 +62,9 @@ def average_time_48(datetimes, pivot=14):
 
 
 def compute_time_averages(data):
-    time_names = ['In Bed', 'Onset', 'Mpos', 'Out Bed']
+    time_names = ['In Bed', 'Onset', 'MPOS', 'Out Bed']
 
-    time_averages = pd.Series()
+    time_averages = pd.Series(dtype=float)
 
     for column_name in time_names:
         time_data = data[column_name]
@@ -72,36 +73,85 @@ def compute_time_averages(data):
     return time_averages
 
 
-def compute_averages(data, subject):
-    normal_data_averages = data.mean()
-    normal_data_averages['TBT'] = timedelta(minutes=normal_data_averages['TBT'])
-    normal_data_averages['TST'] = timedelta(minutes=normal_data_averages['TST'])
+def compute_averages(data):
+    normal_data_averages = data.mean(numeric_only=True)
+    normal_data_averages['TBT'] = (datetime.min + timedelta(minutes=normal_data_averages['TBT'])).time()
+    normal_data_averages['TST'] = (datetime.min + timedelta(minutes=normal_data_averages['TST'])).time()
 
     time_data_averages = compute_time_averages(data)
     averages = normal_data_averages.append(time_data_averages)
-    averages["Subject"] = subject
     return averages
 
 
-def read_reports(reports_files):
-
-    average_data = pd.DataFrame()
+def compute_averages_for_all_reports(reports_files):
+    average_all = pd.DataFrame()  # averages over all days
+    average_we = pd.DataFrame()  # averages over weekend days
+    average_wd = pd.DataFrame()  # averages over weekday days
 
     for report_file in reports_files:
         subject, header_lines = parse_header(report_file)
         data = read_data(report_file, header_lines)
+
+        # there should be seven entries -> seven days a week -> ignore all others
+        if len(data.index) != 7:
+            continue
+
+        # rename some columns
         data.rename(columns={
             "Total Sleep Time (TST)": "TST",
             "Total Minutes in Bed": "TBT",
             "Number of Awakenings": "Awakenings",
-            "Wake After Sleep Onset (WASO)": "WASO"
+            "Wake After Sleep Onset (WASO)": "WASO",
+            "Average Awakening Length": "AAL",
+            "Sleep Fragmentation Index": "SFI"
         }, inplace=True)
-        averages = compute_averages(data, subject)
 
-        if average_data.empty:
-            average_data = pd.DataFrame(columns=averages.index)
-        average_data = average_data.append(averages, ignore_index=True)
-    average_data.set_index("Subject", inplace=True)
+        # compute averages from all days
+        averages = compute_averages(data)
+
+        # compute filter for out bed in the week and not at the weekend
+        weekday = data['Out Bed'].dt.weekday
+        in_week = ((weekday > 1) & (weekday < 7))
+
+        # compute averages for weekday and weekends separately
+        averages_weekday = compute_averages(data[in_week])
+        averages_weekend = compute_averages(data[~in_week])
+
+        # set subject columns
+        averages_weekday["Subject"] = subject
+        averages_weekend["Subject"] = subject
+        averages["Subject"] = subject
+
+        # append result for this subject
+        average_we = average_we.append(averages_weekend, ignore_index=True)
+        average_wd = average_wd.append(averages_weekday, ignore_index=True)
+        average_all = average_all.append(averages, ignore_index=True)
+
+    # use integers for the subjects
+    average_we['Subject'] = average_we['Subject'].astype(int)
+    average_wd['Subject'] = average_wd['Subject'].astype(int)
+    average_all['Subject'] = average_all['Subject'].astype(int)
+
+    # set subjects as index field
+    average_we.set_index("Subject", inplace=True)
+    average_wd.set_index("Subject", inplace=True)
+    average_all.set_index("Subject", inplace=True)
+
+    # print results to console
+    print("averages over all days")
+    print(average_all)
+    average_all = average_all.add_suffix(' (All)')
+    average_all = average_all.add_prefix('Average ')
+
+    average_wd = average_wd.add_suffix(' (Workdays)')
+    average_wd = average_wd.add_prefix('Average ')
+
+    average_we = average_we.add_suffix(' (Weekend)')
+    average_we = average_we.add_prefix('Average ')
+
+    average_data = average_all.join(average_wd).join(average_we).sort_index(axis=1)
+
+    average_data.to_excel("averages.xlsx")
     print(average_data)
 
 
@@ -109,6 +159,7 @@ if __name__ == '__main__':
 
     import sys
     import argparse
+
     parser = argparse.ArgumentParser(description='TODO Description')
 
     parser.add_argument('-r', '--reports', dest="reports_folder",
@@ -127,4 +178,4 @@ if __name__ == '__main__':
     print("Found", len(reports_files), "report files.")
 
     reports_files.sort()
-    read_reports(reports_files)
+    compute_averages_for_all_reports(reports_files)
