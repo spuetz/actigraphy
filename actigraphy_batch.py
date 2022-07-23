7bbc994a42474fbf79e036d06c493337cf4e82f3
Sebastian Pütz <spuetz@uos.de>
2022-07-23 21:25:37 +0200

    compute actigraphy summary

--- actigraphy_batch.py
+++ actigraphy_batch.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 import pyActigraphy
 import os
 import re
@@ -80,6 +80,34 @@ def read_param():
     return args
 
 
+def summary(reader):
+    from pyActigraphy.metrics.metrics import _lmx as lmx
+
+    L5_start, L5 = lmx(reader.binarized_data(4), '5H', lowest=True)
+    L5_midpoint = datetime(2021, 1, 1) + timedelta(hours=2.5) + L5_start
+
+    M10_start, M10 = lmx(reader.binarized_data(4), '10H', lowest=False)
+    M10_midpoint = datetime(2021, 1, 1) + timedelta(hours=5) + M10_start
+
+    data = {
+        'subject': int(reader.name),
+        'Start_time': reader.start_time,
+        'Mask_fraction': reader.mask_fraction(),
+        'Duration': reader.duration(),
+        'ADAT': reader.ADAT(),
+        'L5': L5,
+        'L5 Midpoint': L5_midpoint,  # L5_midpoint.strftime("%H:%M:%S"),
+        'M10': M10,
+        'M10 Midpoint': M10_midpoint,  # M10_midpoint.strftime("%H:%M:%S"),
+        'RA': reader.RA(),
+        'IS': reader.IS(),
+        'IV': reader.IV(),
+        'ISm': reader.ISm(),
+        'IVm': reader.IVm(),
+    }
+    return data
+
+
 if __name__ == '__main__':
 
     # read program parameters
@@ -93,9 +121,48 @@ if __name__ == '__main__':
     wear_time_groups = wear_times.groupby('subject')
 
     # set masks by wear times
+    summaries = []
+
     for reader in raw_reader.readers:
+        print("processing subject {}.".format(int(reader.name)))
         wear_time_mask = get_wear_time_mask(reader, wear_time_groups)
         reader.mask = wear_time_mask
+        reader.mask_inactivity = True
+        summaries.append(summary(reader))
 
+    data = pd.DataFrame(summaries)
+
+    data.set_index("subject", inplace=True)
+    data.index = data.index.astype(int)
+    data.sort_index(inplace=True)
+
+    from read_reports import compute_time_averages
+
+    time_names = ['M10 Midpoint', 'L5 Midpoint']
+    normal_data_averages = data.mean(numeric_only=True)
+    time_data_averages = compute_time_averages(data, time_names, pivot=5)
+    averages = pd.concat([normal_data_averages, time_data_averages])
+
+    print("Averages")
+    print(averages)
+
+    # write averages to excel and csv
+    averages.to_excel("actigraphy_summary_averages.xlsx")
+    averages.to_csv("actigraphy_summery_averages.csv")
+
+    # write actigraphy summary data to html
+    data_html = data.to_html()
+    data_html_file = open("actigraphy_summary.html", "w")
+    data_html_file.write(data_html)
+    data_html_file.close()
+
+    # write actigraphy summary data to csv
+    data.to_csv("actigraphy_summary.csv")
+
+    # write actigraphy summary data to excel
+    data.to_excel("actigraphy_summary.xlsx")
+
+
+    #print(data)
 
 
